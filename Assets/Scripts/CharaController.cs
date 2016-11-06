f24557405e95ad0891deeff15242c5a865bb06ef
Akematty <cgpyuta@gmail.com>
2016-11-06 18:31:33 +0900

    commit
    CG KADAI
    SAMPLE

--- Assets/Scripts/CharaController.cs
+++ Assets/Scripts/CharaController.cs
@@ -1,6 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public struct BeizerPoint
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+}
+
 public class CharaController : MonoBehaviour {
 
 
@@ -80,6 +86,61 @@ public class CharaController : MonoBehaviour {
         if (other.gameObject.tag == "Floor" && !this.isOnGround)
         {
             this.isOnGround = true;
+        }
+    }
+
+    // refはポインタのこと(参照渡し)
+    //          p0座標,       p1座標,           p2座標,       p3座標,       色
+    void bezier(ref int []p0, ref int[] p1, ref int[] p2, ref int[] p3,ref double[]c)
+    {
+        // NUM OF STEP
+        const int STEP = 20;
+
+        // Pa,Pb 座標の定義
+        int[] pa = new int[2];
+        int[] pb = new int[2];
+
+        // Pa <= P0
+        pa[0] = p0[0];
+        pa[1] = p1[1]; 
+
+        for(int i = 0; i < STEP; i++)
+        {
+            // 内分比算出
+            double t = (double)(i+1) / STEP;
+            // 各座標について内分する.
+            for(int j = 0; j < 2; j++)
+            {
+                pb[j] 
+                    = (int)((1 - t) * ((1 - t) * ((1 - t) * p0[j] + 3 * p1[j] * t) + 3 * p2[j] * t * t) + p3[j] * t * t * t);
+            }
+            // 線引く
+            //line(pa[0],pa[1],pb[0],pb[1],ref c)
+            // 次のPa <- Pb
+            pa[0] = pb[0];
+            pa[1] = pb[1];
+        }
+    }
+
+    void Beizer(ref BeizerPoint p0, ref BeizerPoint p1, ref BeizerPoint p2, ref BeizerPoint p3)
+    {
+        BeizerPoint pa, pb;
+        pa = new BeizerPoint();
+        pb = new BeizerPoint();
+        const int STEP = 20;
+
+        pa = p0;
+
+        for(int i = 0; i < STEP; i++)
+        {
+            double t = (double)(i + 1) / STEP;
+            
+            pb.X = (int)((1 - t) * ((1 - t) * ((1 - t) * p0.X + 3 * p1.X * t) + 3 * p2.X * t * t) + p3.X * t * t * t);
+            pb.Y = (int)((1 - t) * ((1 - t) * ((1 - t) * p0.Y + 3 * p1.Y * t) + 3 * p2.Y * t * t) + p3.Y * t * t * t);
+
+            //line(pa[0],pa[1],pb[0],pb[1],ref c);
+
+            pa = pb;
         }
     }
 }
